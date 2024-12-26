df47161366968b68188e84feeddce85d3f91ff87
Ayushpal2006 <git@stackblitz.com>
2024-12-26 20:41:58 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# pintrest_p
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/Ayushpal2006/pintrest_p)
---
+++ src/data/memories.ts
@@ -0,0 +1,34 @@
+import { Memory } from '@/types/memory';
+
+export const memories: Memory[] = [
+  {
+    image: "https://images.unsplash.com/photo-1518568814500-bf0f8d125f46",
+    message: "Every moment with you feels like magic ✨",
+    height: 400
+  },
+  {
+    image: "https://images.unsplash.com/photo-1522673607200-164d1b6ce486",
+    message: "Your smile brightens up my darkest days 🌟",
+    height: 300
+  },
+  {
+    image: "https://images.unsplash.com/photo-1529333166437-7750a6dd5a70",
+    message: "Adventure awaits with you by my side 🌄",
+    height: 350
+  },
+  {
+    image: "https://images.unsplash.com/photo-1516589178581-6cd7833ae3b2",
+    message: "Together is my favorite place to be 💑",
+    height: 380
+  },
+  {
+    image: "https://images.unsplash.com/photo-1422479516648-9b1f0b6e8da8",
+    message: "You make every day special ✨",
+    height: 320
+  },
+  {
+    image: "https://images.unsplash.com/photo-1523438885200-e635ba2c371e",
+    message: "Forever grateful for your love 💕",
+    height: 360
+  }
+];
